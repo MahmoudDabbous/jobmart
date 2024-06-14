89596b21fb3bafe44ac28fc6d435818bd891f6dc
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-06-14 03:40:19 +0300

    feat: add DatabaseModule for PostgreSQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,15 @@
+import { DatabaseModule } from './database/database.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './typeorm/entities/User.js';
-import { Applicant } from './typeorm/entities/Applicant.js';
-import { Education } from './typeorm/entities/Education.js';
-import { Experience } from './typeorm/entities/Experience.js';
-import { Admin } from 'typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      entities: [User, Applicant, Education, Experience, Admin],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env' : '.env.development',
     }),
+    DatabaseModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
