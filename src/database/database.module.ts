8c2fbae8a81c00b229bb004634964771cfb9fd8d
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-06-14 06:11:44 +0300

    refactor: optimize database module structure and entity imports to automatically create table from db entities

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,31 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/User';
-import { Applicant } from './entities/Applicant';
-import { Education } from './entities/Education';
-import { Experience } from './entities/Experience';
-import { Admin } from './entities/Admin';
-import { Job } from './entities/Job';
-import { JobCategory } from './entities/JobCategory';
-import { Application } from './entities/Application';
-import { JobPosition } from './entities/JobPosition';
-import { JobPlatform } from './entities/JobPlatform';
-import { Organization } from './entities/Organization';
-import { Document } from './entities/Document';
-import { ApplicationDocument } from './entities/ApplicationDocument';
-import { ApplicationTest } from './entities/ApplicationTest';
-import { Answers } from './entities/Answer';
-import { ApplicationEvaluation } from './entities/ApplicationEvaluation';
-import { ApplicationStatus } from './entities/ApplicationStatus';
-import { ApplicationStatusChange } from './entities/ApplicationStatusChange';
-import { Interview } from './entities/Interview';
-import { InterviewNote } from './entities/InterviewNote';
-import { Process } from './entities/Process';
-import { ProcessStep } from './entities/ProcessStep';
-import { Recruiter } from './entities/Recruiter';
-import { Step } from './entities/Step';
-import { Test } from './entities/Test';
 
 @Module({
   imports: [
@@ -39,33 +14,7 @@ import { Test } from './entities/Test';
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
         autoLoadEntities: true,
-        entities: [
-          Admin,
-          Answers,
-          Applicant,
-          Application,
-          ApplicationDocument,
-          ApplicationEvaluation,
-          ApplicationStatus,
-          ApplicationStatusChange,
-          ApplicationTest,
-          Document,
-          Education,
-          Experience,
-          Interview,
-          InterviewNote,
-          Job,
-          JobCategory,
-          JobPlatform,
-          JobPosition,
-          Organization,
-          Process,
-          ProcessStep,
-          Recruiter,
-          Step,
-          Test,
-          User,
-        ],
+        entities: ['dist/database/entities/*.js'],
         synchronize: true,
       }),
     }),
