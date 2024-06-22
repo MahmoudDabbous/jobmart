66790cf75401598d3658272b59da70cc90eabaea
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-06-23 02:21:47 +0300

    feat: attach position, category & platforms entity to created job

--- src/modules/jobs/services/jobs/jobs.service.ts
+++ src/modules/jobs/services/jobs/jobs.service.ts
@@ -4,16 +4,38 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Job } from 'src/database/entities/Job';
 import { Repository } from 'typeorm';
+import { PlatformService } from '../platform/platform.service';
+import { PositionService } from '../position/position.service';
+import { CategoryService } from '../category/category.service';
 
 @Injectable()
 export class JobsService {
   constructor(
     @InjectRepository(Job)
     private readonly jobsRepasitory: Repository<Job>,
+    private readonly categoryService: CategoryService,
+    private readonly positionService: PositionService,
+    private readonly platformService: PlatformService,
   ) {}
 
   async create(data: CreateJobDto) {
-    return await this.jobsRepasitory.save(data);
+    const job = this.jobsRepasitory.create(data);
+
+    if (data.jobCategoryId) {
+      job.jobCategory = await this.categoryService.findOne(data.jobCategoryId);
+    }
+
+    if (data.jobPositionId) {
+      job.jobPosition = await this.positionService.findOne(data.jobPositionId);
+    }
+
+    if (data.jobPlatformId) {
+      job.jobPlatform = await this.platformService.findOne(data.jobPlatformId);
+    }
+
+    // TODO: Add process service
+
+    return await this.jobsRepasitory.save(job);
   }
 
   async update(jobId: number, data: UpdateJobDto) {
